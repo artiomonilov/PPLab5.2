0090241cb930ca71568ce3747d2f1aefc73c6ab2
ARTIOM\artio <artiomonilov2005@gmail.com>
2026-03-22 15:20:18 +0200

    Deschidere aplicatie

---
+++ main.py
@@ -0,0 +1,19 @@
+﻿import sys
+import random
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QFrame
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Random citat")
+        self.resize(600, 300)
+
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
