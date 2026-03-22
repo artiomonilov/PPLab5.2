755c0f89411e0b71f3085b9ee30e4d441bf5d53a
ARTIOM\artio <artiomonilov2005@gmail.com>
2026-03-22 15:39:25 +0200

    Deschidere aplicatie

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Random citat")
+        self.setWindowTitle("Preia citat aleatoriu")
         self.resize(600, 300)
+        
+        citat_zilnic = self.alege_citat("citate.txt")
 
-    
+        print(citat_zilnic)
+
+    def randomCitat(self, locatie):
+        if not os.path.exists(locatie):
+            return "Fișierul citate.txt nu a putut fi găsit."
+        try:
+            with open(locatie, "r", encoding="utf-8") as file:
+                linii = [linie.strip() for linie in file.readlines() if linie.strip()]
+                print(linii)
+            if not linii:
+                return "Fișierul de citate este gol."
+            return random.choice(linii)
+        except Exception as e:
+            return f"Eroare la citirea fișierului: {str(e)}"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
